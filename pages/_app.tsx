6d0dac2d81b14a2c36c5badd32b6447af5dd4453
Dprogramm3r <32206379+Dprogramm3r@users.noreply.github.com>
2023-01-07 15:00:27 +0100

    entry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@rainbow-me/rainbowkit/styles.css";
+import {configureChains, WagmiConfig,createClient  } from 'wagmi';
+import { avalanche, bsc,optimism, arbitrum, mainnet,polygon, localhost } from '@wagmi/core/chains'
+import { publicProvider } from '@wagmi/core/providers/public'
+import { getDefaultWallets } from '@rainbow-me/rainbowkit';
+import {
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+const {chains, provider} = configureChains(
+  [mainnet, avalanche, bsc,polygon, localhost,optimism, arbitrum],
+  [publicProvider()]
+);
+
+const {connectors} = getDefaultWallets({
+  appName: "multiwallet connect",
+  chains,
+})
+const wagmiClient = createClient({
+  autoConnect:true,
+  connectors,
+  provider
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+      <Component {...pageProps} />
+      </RainbowKitProvider>
+  </WagmiConfig>
+
+  )
 }
